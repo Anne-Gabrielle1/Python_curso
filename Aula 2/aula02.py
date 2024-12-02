61224dbb8a1e2be7fe8da213cae57ec186deddaa
Anne-Gabrielle1 <annegabriellesan23@gmail.com>
2024-12-02 10:00:45 -0300

    aprendi o que é uma variavel

---
+++ Aula 2/aula02.py
@@ -0,0 +1,8 @@
+print("olá,mundo")
+
+nome_usuario = "Anne Gabrielle" #tipo -> str = string
+idade_usuario = 19 #tipo -> int = inteiro 
+altura_usuario = 1.55 #tipo -> float = flutiante numero decimal
+maior_de_idade = True #tipo -> bool = boolean
+
+print("Seja bem-vindo", nome_usuario, "sua idade é", idade_usuario, "sua altura é", altura_usuario,"você é maior de idade?", maior_de_idade )
